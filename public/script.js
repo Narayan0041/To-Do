88cd3989a93ba3bd7e0a66ad2200282849000fc6
Shiv@9089 <yadavshiv0041@gmail.com>
2023-10-08 21:19:11 +0530

    develop the different section and bugs fixes

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const studentDetail = [ ]
+const studentDetail = []
 
 // -----------------------------------------genrate_password start here------------------------------------------------
 let char = ["@", "#", "&", "!", "#", "%"]
@@ -17,31 +17,31 @@ function myFunction() {
     } else {
         console.log("your should more then 10 digit")
     }
-    let genrate_pass = document.getElementById("passGenrator").innerHTML = "<h2>Suggestion strong password</h2>" + final;
-
-
+    let genrate_pass = document.getElementById("passGenrator")
+    genrate_pass.innerHTML = "<h2>Suggestion strong password</h2>" + final;
+    genrate_pass.style.marginLeft ="2rem";
+    genrate_pass.style.marginTop ="1rem"
 }
+// ---------------------------------------sign in link--------------------------------------------
+function signIn() {
+    window.location.href = "login.html"
+}
+
 // -------------------------------------------genrate_password end here-------------------------------------------------
 
 function register(event) {
     let firstName = document.getElementById("userName").value.trim();
-    let lastName = document.getElementById("lastName").value.trim();
+    // let lastName = document.getElementById("lastName").value.trim();
     let email = document.getElementById("email").value.trim();
     let password = document.getElementById("password").value.trim();
-    // console.log(firstName,lastName,email,password)
 
     //for FirstName validation
     if (firstName == "") {
         document.getElementById("error").innerText = "firstName should not be empty !!";
         document.getElementById("error").style.color = "red";
         document.getElementById("error").style.fontSize = ".9rem";
-    }
-
-    //for lastName validation
-    if (lastName == "") {
-        document.getElementById("lastNameError").innerText = "lastName should not be empty!!";
-        document.getElementById("lastNameError").style.color = "red";
-        document.getElementById("lastNameError").style.fontSize = ".9rem";
+        document.getElementById("error").style.marginLeft = "1.2rem";
+        document.getElementById("error").style.marginTop = ".5rem";
     }
 
     //for email validation
@@ -49,6 +49,8 @@ function register(event) {
         document.getElementById("emailError").innerText = "Email should not be empty!!";
         document.getElementById("emailError").style.color = "red";
         document.getElementById("emailError").style.fontSize = ".9rem";
+        document.getElementById("emailError").style.marginLeft = "1.2rem";
+        document.getElementById("emailError").style.marginTop = ".5rem";
     }
 
     //for password validation
@@ -57,6 +59,8 @@ function register(event) {
         document.getElementById("passwordError").innerText = "password should not be empty and more 8 digit!!";
         document.getElementById("passwordError").style.color = "red";
         document.getElementById("passwordError").style.fontSize = ".9rem";
+        document.getElementById("passwordError").style.marginLeft = "1.5rem";
+        document.getElementById("passwordError").style.margintop = ".7rem";
     }
     if (password.length <= 8) {
         document.getElementById("passwordError").innerHTML = "Password should more thane 8 digit";
@@ -64,9 +68,10 @@ function register(event) {
         console.log("Password should more than 8 digit");
     }
 
-    if (firstName && lastName && email && password) {
+    //write the condition for register 
+    if (firstName && email && password) {
         var id = studentDetail.length + 1;
-        studentDetail.push({ firstName: firstName, lastName: lastName, email: email, password: password, id: id })
+        studentDetail.push({ firstName: firstName, email: email, password: password, id: id })
         let str = JSON.stringify(studentDetail);
         localStorage.setItem("studentDetail", str)
         alert("You are the Register Succesfully")
@@ -80,7 +85,6 @@ function register(event) {
 // clear function 
 function clear() {
     let firstName = document.getElementById("userName").value = "";
-    let lastName = document.getElementById("lastName").value = "";
     let email = document.getElementById("email").value = "";
     let password = document.getElementById("password").value = "";
     document.getElementById("passGenrator").style.display = "none"
@@ -104,14 +108,18 @@ function login() {
     if (LoginEmail == "") {
         document.getElementById("emailError").innerText = "Email should not be empty!!";
         document.getElementById("emailError").style.color = "red";
-        document.getElementById("emailError").style.fontSize = ".9rem";
+        document.getElementById("emailError").style.fontSize = ".8rem";
+        document.getElementById("emailError").style.marginLeft = "2.7rem";
+        document.getElementById("emailError").style.margintop = "1.2rem";
     }
     //for password validation
     else if (LoginPassword == "") {
         console.log("pass")
         document.getElementById("passwordError").innerText = "password should not be empty and more 8 digit!!";
         document.getElementById("passwordError").style.color = "red";
-        document.getElementById("passwordError").style.fontSize = ".9rem";
+        document.getElementById("passwordError").style.fontSize = ".7rem";
+        document.getElementById("passwordError").style.marginLeft = "2.7rem";
+        document.getElementById("passwordError").style.margintop = "1.2rem";
     }
     else if (LoginPassword.length <= 8) {
         document.getElementById("passwordError").innerHTML = "Password should more thane 8 digit";
@@ -119,11 +127,11 @@ function login() {
         console.log("Password should more than 8 digit");
     }
 
-   
+    // check the condition wether the user are exits or not 
     for (let i = 0; i < final.length; i++) {
         if (final[i].email == LoginEmail && final[i].password == LoginPassword) {
             console.log("Password should match")
-            window.location.href="index.html"
+            window.location.href = "index.html"
         }
         else {
             console.log("match should not match")
@@ -188,17 +196,17 @@ function login() {
 // }
 
 let students_section = document.getElementById("students-box-section");
+
 let studentsData = localStorage.getItem("studentDetail")
 let final = JSON.parse(studentsData)
 
 let window1 = window.addEventListener("load", () => {
-    var html = "<table border='1|1'  class='table'>"
+    var html = "<table border='1|1' class='table'>"
     setTimeout(() => {
         html += "<thead>";
         html += "<tr>";
         html += "<td>" + 'SNo.' + "</td>";
         html += "<td>" + 'first Name' + "</td>";
-        html += "<td>" + 'last Name' + "</td>";
         html += "<td>" + 'Email' + "</td>";
         html += "<td>" + 'Action' + "</td>";
         html += "</tr>"
@@ -208,7 +216,6 @@ let window1 = window.addEventListener("load", () => {
             html += "<tr>";
             html += "<td>" + sno + "</td>";
             html += "<td>" + final[j].firstName + "</td>";
-            html += "<td>" + final[j].lastName + "</td>";
             html += "<td>" + final[j].email + "</td>";
             html += "<td>" + `<button type="button" class="btn btn-danger" onclick="removeItem(${final[j].id})">Remove</button>` + "</td>";
             html += "</tr>";
@@ -216,20 +223,31 @@ let window1 = window.addEventListener("load", () => {
         html += "</table>";
         students_section.innerHTML = html
 
+        // noCustomer --------------------------------
+        let Ncustomer = document.getElementById("noCustomer");
+        Ncustomer.innerHTML = final.length;
+        console.log(Ncustomer)
+
     }, 200)
     console.log("page is loaded")
 
 })
 function removeItem(remove) {
     let id = remove;
-    removeDataFromLocalStorage(id);
-    console.log("click hu remove btn", id)
+    let conf = confirm("Do you want Delete the user");
+    if (conf) {
+        removeDataFromLocalStorage(id);
+        console.log("click hu remove btn", id)
+    }
 }
 
 // Function to remove data from localStorage
 function removeDataFromLocalStorage(id) {
     const updatedData = final.filter(item => item.id !== id);
     localStorage.setItem('studentDetail', JSON.stringify(updatedData));
+
+
+
 }
 
 //   ---------------------------------------------search-option---------------------------------------------
